b35cbc70b85129720761533369f9d473ca312e0c
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-04-22 11:51:36 -0500

    testcode

---
+++ test_container.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+singularity run --contain --cleanenv \
+	--bind INPUTS:/INPUTS \
+	--bind $OUTPUTS:/OUTPUTS \
+	afnitest.simg \
+	@afni_refacer_run \
+	-input /INPUTS/t1.nii.gz \
+	-mode_all \
+	-prefix /OUTPUTS/t1
